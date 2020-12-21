66da41979fc926c8ff83137e0f48ea52932ebfde
aakhmetzyanov <aakhmetzyanov@diasoft.ru>
2020-12-21 17:59:36 +0300

    init commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+e
+5src/main/java/com/example/three/ThreeApplication.java,e\e\eef1ed3aed93da8bc6eae32f848a1093f89fb73d
+q
+Asrc/main/java/com/example/three/controller/MessageController.java,8\5\85a4c348d915fcefe3719c5e2d3e263bb5832f65
+p
+@src/main/java/com/example/three/exceptions/NotFoundExeption.java,d\b\db2f0c4d7f94d70c156cb3c113beb58ab283323b
---
+++ src/main/java/com/example/three/controller/MessageController.java
@@ -0,0 +1,68 @@
+package com.example.three.controller;
+
+import com.example.three.exceptions.NotFoundExeption;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private int counter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<>() {{
+        add(new HashMap<>() {{
+            put("id", "1");
+            put("text", "First message");
+        }});
+        add(new HashMap<>() {{
+            put("id", "2");
+            put("text", "Second message");
+        }});
+        add(new HashMap<>() {{
+            put("id", "3");
+            put("text", "Third message");
+        }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(String id) {
+        return messages.stream()
+                .filter(messages -> messages.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundExeption::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+            message.put("id", String.valueOf(counter++));
+            messages.add(message);
+            return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> modify (@PathVariable String id, @RequestBody Map<String, String> message){
+        Map<String, String> messageFromDB = getMessage(id);
+        messageFromDB.putAll(message);
+        messageFromDB.put("id", id);
+        return message;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Map<String, String> mesage = getMessage(id);
+        messages.remove(mesage);
+    }
+}
